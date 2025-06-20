bbfbec369c38b10309e6a611cd3168cf6b7ddc18
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-06-20 13:45:20 -0400

    first commit

---
+++ h2inmemorydatase/src/main/java/com/numeriquepro/h2inmemorydatase/H2inmemorydataseApplication.java
@@ -0,0 +1,13 @@
+package com.numeriquepro.h2inmemorydatase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2inmemorydataseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2inmemorydataseApplication.class, args);
+	}
+
+}
