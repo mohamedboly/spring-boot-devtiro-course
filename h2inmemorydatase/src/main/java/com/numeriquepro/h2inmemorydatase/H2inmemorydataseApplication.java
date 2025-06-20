e7e6966dcc5748a4c78d04104f9d3c3e8afe8b02
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-06-20 14:52:15 -0400

    h2 InMemory database

--- h2inmemorydatase/src/main/java/com/numeriquepro/h2inmemorydatase/H2inmemorydataseApplication.java
+++ h2inmemorydatase/src/main/java/com/numeriquepro/h2inmemorydatase/H2inmemorydataseApplication.java
@@ -1,13 +1,35 @@
 package com.numeriquepro.h2inmemorydatase;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class H2inmemorydataseApplication {
+@Log
+public class H2inmemorydataseApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2inmemorydataseApplication.class, args);
 	}
 
+	private final DataSource dataSource;
+
+	public H2inmemorydataseApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+
+
+	@Override
+	public void run(final String... args) {
+		System.out.println("dfghjk boly");
+		log.info("Datasource: " + dataSource.toString());
+
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("SELECT 1");
+	}
 }
