1dc6cac9346932c77c3236e3b7b0d014c45f2311
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-06-21 16:14:33 -0400

    JDBC Template Configuration

---
+++ postegresqldatabasejdbctemplate/src/main/java/com/numeriquepro/postegresqldatabasejdbctemplate/PostegresqldatabasejdbctemplateApplication.java
@@ -0,0 +1,13 @@
+package com.numeriquepro.postegresqldatabasejdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostegresqldatabasejdbctemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostegresqldatabasejdbctemplateApplication.class, args);
+	}
+
+}
---
+++ postegresqldatabasejdbctemplate/src/test/java/com/numeriquepro/postegresqldatabasejdbctemplate/PostegresqldatabasejdbctemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numeriquepro.postegresqldatabasejdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostegresqldatabasejdbctemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
